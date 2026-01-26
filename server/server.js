907bd564f25f0df10520fb9781c75c1d88ac9572
Vince <vincedimatatac@gmail.com>
2026-01-27 00:35:32 +0800

    MongoDB config setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './configs/db.js';
 
 const app = express();
+
+// Connect to MongoDB
+await connectDB();
 
 // Middleware
 app.use(express.json());
