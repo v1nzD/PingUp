ca19bab947bc4c47601da0a10b73972a9bb35152
Vince <vincii09@ahoo.com>
2026-01-27 17:24:08 +0800

    Created a user schema model + inggest setup and created function to sync user data from clerk

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import { inngest, functions } from "./inggest/index.js";
 
 const app = express();
 
