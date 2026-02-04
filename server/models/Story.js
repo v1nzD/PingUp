af5c48407eb710ecee64c15d87bb4a425778f975
Vince <vincedimatatac@gmail.com>
2026-02-04 15:35:09 +0800

    Story model + story api routes + created inngest function to delete story after 24 hours

---
+++ server/models/Story.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const storySchema = new mongoose.Schema(
+    {
+        user: { type: String, ref: "User", required: true },
+        content: { type: String },
+        media_url: [{ type: String }],
+        media_type: { type: String, enum: ['text', 'image', 'video']},
+        view_count: [{ type: String, ref: "User" }],
+        background_color: { type: String }
+    }, { timestamps: true, minimize: false }
+)
+
+const Story = mongoose.model("Story", storySchema);
+
+export default Story;
