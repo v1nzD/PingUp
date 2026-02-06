d24c506cc0276f94ba70f98b362c3261db311cc7
Vince <vincii09@ahoo.com>
2026-02-06 18:36:39 +0800

    inngest function to send notification for unseen messages

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,6 +12,7 @@ import {
 } from "../controllers/userController.js";
 import { protect } from "../middleware/auth.js";
 import { upload } from "../configs/multer.js";
+import { getUserRecentMessages } from "../controllers/messageController.js";
 
 const userRouter = express.Router();
 
@@ -33,5 +34,6 @@ userRouter.post("/connect", protect, sendConnectionRequest);
 userRouter.post("/accept", protect, acceptConnectionRequest);
 userRouter.get("/connections", protect, getUserConnections);
 userRouter.post('/profiles', protect, getUserProfile)
+userRouter.get('/recent-messages', protect, getUserRecentMessages)
 
 export default userRouter;
