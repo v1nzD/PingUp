024f6e2d104a113f3ee2fbc3688c195fe0a1e53f
Vince <vincedimatatac@gmail.com>
2026-02-06 18:36:39 +0800

    inngest function to send notification for unseen messages

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import Connection from "../models/Connection.js";
 import sendEmail from "../configs/nodemailer.js";
 import Story from "../models/Story.js";
+import Message from "../models/Message.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -138,5 +139,41 @@ const deleteStory = inngest.createFunction(
   }
 )
 
+const sendNotificationOfUnseenMessages = inngest.createFunction(
+  {id: "send-unseen-messages-notification"},
+  {cron: "TZ=America/New_York 0 9 * * *"}, // everyday at 9am
+  async ({step}) => {
+    const messages = await Message.find({seen: false}).populate('to_user_id');
+    const unseenCount = {}
+
+    messages.map(message => {
+      unseenCount[message.to_user_id] = (unseenCount[message.to_user_id._id] || 0) + 1;
+    })
+
+    for (const userId in unseenCount){
+      const user = await User.findById(userId);
+
+      const subject = `You have ${unseenCount[userId]} unseen messages`
+
+      const body = `
+      <div style="font-family: Arial, sans-serif; padding: 20px;">
+        <h2>Hi ${user.full_name},</h2>
+        <p>You have ${unseenCount[userId]} unseen messages</p>
+        <p>Click <a href="${process.env.FRONTEND_URL}/messages" style="color:#10b981;">here</a> to view them</p>
+        <br/>
+        <p>Thanks,<br/>PingUp - Stay Connected</p>
+      </div>`;
+
+      await sendEmail({
+        to: user.email,
+        subject,
+        body
+      })
+    }
+
+    return {message: "Notification sent."}
+  }
+)
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion, sendNewConnectionRequestReminder];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion, sendNewConnectionRequestReminder, deleteStory, sendNotificationOfUnseenMessages];
